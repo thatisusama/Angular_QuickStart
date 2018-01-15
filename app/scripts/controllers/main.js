245c1e4024ce2001ecf3fdcca65ffd03881dd7d0
Muhammad Usama <usama6000@gmail.com>
2018-01-15 20:04:03 +0500

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('helloApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.Usama = "That is Usama";
+  });
